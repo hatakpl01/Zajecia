ffee565cd2d0b57510c177bd84e0aed03b5d54c7
Przemek <przemyslawjakubowski@gmail.com>
2021-02-18 14:34:26 +0100

    Lekcja1

--- src/lekcja1/Main.java
+++ src/lekcja1/Main.java
@@ -3,5 +3,65 @@ package lekcja1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//  =========================== ZMIENNE ==================================
+//        Co to są zmienne? Są to pojemniki na dane.
+//        W Javie jest wiele różnych pojemników. Tak jak w normalnym życiu.
+//        Pokazówka........... z dwoma pojemnikami
+//
+//        Tworząc zmienną:
+//              <typ> <nazwa> = <wartość>;
+//              int liczba = 5;
+//              String tekst = "Imie";
+
+//        Stwórzmy zmienną typu całkowitego i nadajmy jej wartość 10.
+//        Wypiszmy ją na ekran.
+//        Stwórzmy zmienną do przechowywania tekstu. Wpisz do niej swoje imię.
+//        Wypiszmy ją na ekran.
+
+
+
+// ============================ TYPY DANYCH DLA LICZB =======================
+
+
+//        Pokazówka ze szklanymi pojemnikami....
+// CALKOWITE
+// byte 1 byte  -128 to 127
+// short 2 bytes  -32,768 to 32,767
+// int 4 bytes -2,147,483,648 to 2,147,483,647
+// long 8 bytes -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//
+// ZMIENNOPRZECINKOWE
+// float 4 bytes 7 decimal digits
+// double 8 bytes 16 decimal digits
+
+//        int liczba = 4;
+//        System.out.println(liczba);
+//        byte bajcik = 1;
+//        int intigerek = 1;
+//
+//        System.out.println(bajcik);
+//        System.out.println(intigerek);
+
+//        bajcik = 129;
+//        intigerek = 129;
+
+//        float floacik = 0.123F;
+//        double dablik = 0.123;
+//
+//        System.out.println(floacik);
+//        System.out.println(dablik);
+//
+//        floacik = 0.123456789F;
+//        dablik = 0.123456789;
+//
+//        System.out.println("float: " + floacik);
+//        System.out.println("double: " + dablik);
+
+//        System.out.println(2 - 1.1);
+
+
+
+
+//  Jak starczy czasu: wypisywanie zmiennych w ramach jednego sout'a
     }
 }
---
+++ src/lekcja1/Zadanie.java
@@ -0,0 +1,11 @@
+package lekcja1;
+
+public class Zadanie {
+    public static void main(String[] args) {
+//        Stwórz 3 zmienne do przechowywania liczb całkowitych. Nadaj im wartości 5, 10, 15. Wyświetl je na ekranie.
+//        Stwórz 2 zmienne do przechowywania tekstu. Zapisz do nich swoje imie i nazwisko.
+//        Stwórz 2 zmienne do przechowywania wartości PI. Jedna typu float, druga double. Uzupełnij je do maksymalnej
+//          możliwej precyzji. Wyświetl je na ekranie.
+//
+    }
+}
