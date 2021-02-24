ae6c1d9b132aa7ebdad69b3f0181a41160995b34
Przemek <przemyslawjakubowski@gmail.com>
2021-02-24 15:56:06 +0100

    Lekcja2

---
+++ src/lekcja2/Main.java
@@ -0,0 +1,65 @@
+package lekcja2;
+
+public class Main {
+    public static void main(String[] args) {
+//        1. char - pojedynczy znak - z tego składają się Stringi
+//        char znak = 'a';
+
+//        2. boolean - prawda/fałsz
+//        boolean logika = true;
+//        logika = false;
+
+//        3. Co to są te // ?
+//        Komentarze w kodzie - nie są w żaden sposób interpretowane.
+
+// komentarz pojedynczej linijki Ctrl + /
+//        komentarz wielolinijkowy
+
+
+        /*
+        lalalal
+                lalala
+                lalala
+                        lalala
+                        lalala
+                                lalala
+         */
+
+//        3. Nazewnictwo zmiennych
+//          nazwa zmiennej musi zaczynać się literą i składać z liter i cyfr
+
+//        double wyplata;
+//        int liczbaDniWolnychOdPracyW2021RokuPoOdliczeniuSwiatNarodowych;
+//        long populacjaLudnosciNaZiemi;
+//        boolean zrobione;
+
+//        4. zmienne trzeba zainicjalizować
+//        int wiek;
+//        System.out.println(wiek);
+
+//        deklarację zmiennych można przeplatać z regularnym kodem
+//        dobry styl programowania, to deklarowanie zmiennych jak najbliżej ich użycia
+//        boolean prawdaCzyFalsz = true;
+//        System.out.println("Coś tam coś tam" + prawdaCzyFalsz);
+//
+//        String jakisTekst = "jakis tam tekst";
+//        System.out.println(jakisTekst);
+
+//        5. Stałe
+//          stałe oznaczamy słówkiem final i piszemy dużymi literami
+
+//        final String IMIE = "Przemek";
+//        System.out.println("Twoje imie to " + IMIE);
+//
+//        IMIE = "Staszek";
+
+//        6. Operatory: +, -, *, / , %
+//        System.out.println("1 + 2 = " + (1 + 2));
+//        System.out.println("1 - 2 = " + (1 - 2));
+//        System.out.println("2 * 2 = " + (2 * 2));
+//        System.out.println("9 / 3 = " + (9 / 3));
+//        System.out.println("5 % 2 = " + (5 % 2));
+
+//        7. Napiszmy program liczący pole prostokąta. Zadeklaruj zmienne a,b, rezutlat
+    }
+}
---
+++ src/lekcja2/Zadanie.java
@@ -0,0 +1,8 @@
+package lekcja2;
+
+public class Zadanie {
+//    1. Napisz program liczący pole trójkąta. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
+//    2. Napisz program liczący pole kwadratu. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
+//    3*. Napisz program liczący pole koła. Zadeklaruj odpowiednią zmienną do przechowywania promienia oraz rezultatu.
+//          Pomyśl jak przechowywać PI i czy możnaby tej wartości nie wpisywać "z palca"?
+}
