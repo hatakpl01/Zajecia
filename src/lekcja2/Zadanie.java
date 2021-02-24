509dbe572469ca29bd84054a02bda0deb97a2a72
Przemek <przemyslawjakubowski@gmail.com>
2021-02-24 16:02:38 +0100

    Lekcja2

--- src/lekcja2/Zadanie.java
+++ src/lekcja2/Zadanie.java
@@ -1,8 +1,11 @@
 package lekcja2;
 
 public class Zadanie {
-//    1. Napisz program liczący pole trójkąta. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
-//    2. Napisz program liczący pole kwadratu. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
-//    3*. Napisz program liczący pole koła. Zadeklaruj odpowiednią zmienną do przechowywania promienia oraz rezultatu.
+
+//    1. Napisz kod, który doda to siebie dwie liczby i wypisze wynik dodawania
+//    2. Napisz kod, który podzieli dwie liczby i wypisze wynik dzielenia
+//    3. Napisz program liczący pole trójkąta. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
+//    4. Napisz program liczący pole kwadratu. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
+//    5*. Napisz program liczący pole koła. Zadeklaruj odpowiednią zmienną do przechowywania promienia oraz rezultatu.
 //          Pomyśl jak przechowywać PI i czy możnaby tej wartości nie wpisywać "z palca"?
 }
