886365c042ded598411f8342759bf9b2ae428d4e
Przemek <przemyslawjakubowski@gmail.com>
2021-03-05 22:07:16 +0100

    Lekcja 3 po lekcji

--- src/lekcja3/Main.java
+++ src/lekcja3/Main.java
@@ -5,42 +5,79 @@ public class Main {
 
 //    1. Używanie kodu napisanego przez innych
         double pi = Math.PI;
+        double innePi = Math.PI;
         System.out.println(pi);
 
         double potega = Math.pow(3, 2);
         System.out.println(potega);
 
         String imie = "Przemek";
-        boolean czyJestE = imie.contains("e");
-        System.out.println(czyJestE);
-        System.out.println(imie.length());
+        boolean czyMaE = imie.contains("e");
+        System.out.println(czyMaE);
+//        boolean czyJestE = imie.contains("e");
+//        System.out.println(czyJestE);
+//        System.out.println(imie.length());
+
+
 //  2. Instrucja warunkowa
 
-//        boolean warunek = true;
-//        warunek = false;
-//
-//        if (warunek) {
-//            System.out.println("Prawda");
-//        } else {
-//            System.out.println("Fałsz");
-//        }
-//
-//        int dwa = 2;
-//        int szesc = 6;
-//
-//        System.out.println(dwa > szesc);
-//        System.out.println(szesc > dwa);
-//
-//        if (dwa > szesc) {
-//            System.out.println("Jest tak");
-//        } else {
-//            System.out.println("Nie jest tak");
-//        }
+        boolean warunek = true;
+        warunek = false;
 
-//          3. Napisz program, który na podstwie długości twojego imienia wypisze odpowiedni komunikat.
-//        Jeśli długość imienia jest dłuższa niż 8 znaków ma wypisać "Dość długie imie"
+        if (warunek) {
+            System.out.println("Prawda");
+        } else {
+            System.out.println("Fałsz");
+        }
+
+        int liczba1 = 6;
+        int liczba2 = 6;
+
+        System.out.println(liczba1 > liczba2);
+        System.out.println(liczba2 > liczba1);
+
+        if (liczba1 > liczba2) {
+            System.out.println("True");
+        }
+        else if (liczba1 == liczba2) {
+            System.out.println("Tu nie bedziemy");
+        }
+        else {
+            System.out.println("False");
+        }
+
+//        Stwórz 2 zmienne typu int. Do jednej wpisz 10, do drugiej 15
+//        Stwórz instrukcję warunkową sprawdzającą czy 10 jest większe od 15
+//        i wypisującą w tym przypadku 10 jest większe od 15,
+//        a w przeciwnym razie 10 nie jest większe od 15
+
+        String imie2 = "Przemek";
+        int dlugoscImienia = imie2.length();
+//          3. Napisz program, który na podstwie długości Twojego
+//          imienia wypisze odpowiedni komunikat.
+//        Jeśli długość imienia jest dłuższa niż 6 znaków ma wypisać
+//        "Dość długie imie"
 //        W przeciwnym wypadku "Dość krótkie imie"
 
+
+        double a = 3;
+        double b = 1;
+        double c = 10;
+
+        double delta;
+
+        System.out.println("Równanie ma postać " + a + "*x^2 + " + b + "*x + " + c);
+
+        delta = Math.pow(b, 2) - 4 * a * c;
+
+
+
+
+
+
+
+
+//        ======================== NIE BYLO ========================
 //          4. Konwersja typów numerycznych
 
 //        int liczbaInt = 5;
--- src/lekcja3/Zadanie.java
+++ src/lekcja3/Zadanie.java
@@ -3,4 +3,8 @@ package lekcja3;
 public class Zadanie {
     //// Napisz program rozwiązujący równania kwadratowe (o ile istnieje)
 ////        https://www.matemaks.pl/rownania-kwadratowe-w-postaci-ogolnej.html
+
+//    Losujemy liczbę z zakresu 0 - 10 000 i sprawdzamy czy jest większa od 10 i wypisujemy to,
+//    Większa od 100 i wypisujemy
+//    Większa od 1000 i wypisujemy
 }
