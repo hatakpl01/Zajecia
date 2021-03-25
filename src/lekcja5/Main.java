27df06c2b812ce3d3aa6da8ca175bae56ee532f7
Przemek <przemyslawjakubowski@gmail.com>
2021-03-25 15:24:49 +0100

    Lekcja 5 po kecji

--- src/lekcja5/Main.java
+++ src/lekcja5/Main.java
@@ -1,5 +1,8 @@
 package lekcja5;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,24 +10,46 @@ public class Main {
 //    break;
 //    continue;
 
-        int i = 0;
+//        int i = 0;
+//
+//        while (i < 10) {
+//            i++;
+//            if (i % 2 == 0) {
+//                continue;
+//            }
+//            System.out.println("i = " + i);
+//        }
+//
+//        System.out.println("=========================");
+//
+//        int j = 0;
+//        while (j < 10) {
+//            if (j == 7) break;
+//            j++;
+//            System.out.println("j = " + j);
+//        }
 
-        while (i < 10) {
-            i++;
-            if (i % 2 == 0) {
-                continue;
+        Random random = new Random();
+        int wylosowana = random.nextInt(100);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe");
+        int guess = scanner.nextInt();
+        if (guess < wylosowana) {
+            System.out.println("Za mało");
+        } else if (guess > wylosowana) {
+            System.out.println("Za duzo");
+        }
+        while (guess != wylosowana) {
+            System.out.println("Podaj kolejna liczbe");
+            guess = scanner.nextInt();
+
+            if (guess < wylosowana) {
+                System.out.println("Za mało");
+            } else if (guess > wylosowana) {
+                System.out.println("Za duzo");
             }
-            System.out.println("i = " + i);
         }
-
-        System.out.println("=========================");
-
-        int j = 0;
-        while (j < 10) {
-            if (j == 7) break;
-            j++;
-            System.out.println("j = " + j);
-        }
+        System.out.println("GRATULACJE!");
 
 //        2. Robimy wspólnie zadanie domowe
 //        3. W zadaniu domowym zmodyfikować kod tak, żeby wyświetlana była liczba prób. Ich całkowita ilość to 10.
